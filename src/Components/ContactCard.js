3cb80cd441606020ffd64019c0e3381867850663
pavankumarvn1518 <pavan.1518am@gmail.com>
2024-03-22 11:53:56 +0530

    first commit

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,1 +1,21 @@
 import React from "react";
+
+const CardContact = () => {
+    const{id,name,email} =props.contact;
+    return(
+        <div className="item">
+                <div className="content">
+                    <div className="header">{contact.name}</div>
+                    <div>{contact.email}</div>
+                </div>
+                <i className="trash alternate outline icon" 
+                style={{color:"red",marginTop:"7px"}}
+                
+                >
+
+                </i>
+
+            </div>
+
+    );
+}
--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,19 +1,12 @@
 import React from "react";
+import CardContact from "./ContactCard";
 
 
 const contactList = (props) => {
     console.log(props); 
     const renderContactList = props.contacts.map((contact) => {
         return(
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className="trash alternate outline icon"></i>
-
-            </div>
-
+           <contactCard ></contactCard>
         );
 
 
